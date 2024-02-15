847763ade605226bb306e496b1e187416a7ca10f
JeremyGYJ <139707228+JeremyGYJ@users.noreply.github.com>
2024-02-15 13:18:45 +0000

    Add function App

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
-function Timer (){
-   const [count, setCount] = useState(10);
+function Timer() {
+  const [count, setCount] = useState(10);
 
-  useEffect(() =>{
-    const timerId = setInterval (() =>{
-      setCount(count - 1);}, 1000);
-    }
-           ,[]);
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setCount(count - 1);
+    }, 1000);
+  }
+    , []);
 
   return <div> Time Remaining: {count}s </div>
- }
+}
 
 export default function App() {
-  return (
-    <main>
+  const [showTimer, setShowTimer] = useState(true);
+  const toggleTimer = () => setShowTimer(!showTimer)
 
-    </main>
+  return (
+    <div>
+      <h1>React Timer App</h1>
+      {showTimer && <Timer />}
+      <button onClick={toggleTimer}>
+        {showTimer ? 'HideTimer' : 'ShowTimer'}
+      </button>
+    </div>
+
   )
 }
